0b6144faa8bbac13635f5b6813fc6a5925a0b750
John Park <john.suk@descomplica.com.br>
2019-08-15 18:07:51 -0300

    Finalizing hero component structure

--- project/src/components/hero/index.js
+++ project/src/components/hero/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './index.scss'
 
+import descomplicaLogo from '../../assets/d-descomplica.svg'
+
 export default class Hero extends React.Component {
 
   render() {
     return(
+      <React.Fragment>
         <div className='container-hero' >
-            <h1 className='h1-title-hero'>HERO</h1>
-            <h2 className='h2-subtitle-hero'>DANNY DEVITO!!! <br /> DANNY DEVITO!!!</h2>
-            <div className='cta-hero'>Click Here</div>
+          <div className='header-hero'>
+            <img className='desco-logo' alt='logo-descomplica' src={descomplicaLogo} />
+            <button className='login-button'>Já sou aluno</button>
+          </div>
+          <h1 className='h1-title-hero'>HERO TITLE</h1>
+          <h2 className='h2-subtitle-hero'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+          <button className='cta-hero' onClick={this.props.onClick}>Click Here</button>
         </div>
+      </React.Fragment>
     );
   }
 }
