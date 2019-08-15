2f2aa9cb85e498726ec77b192584930a84cd569b
Vinicius Zamith <vmarqueszamith@gmail.com>
2019-08-15 13:47:40 -0300

    Creating some more components

--- project/src/components/hero/index.js
+++ project/src/components/hero/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
 import './index.scss'
 
-export default function Hero() {
-    return(
-        <div className='container' >
-            <h1 className='first-title'>WE RULE THEN ALL</h1>
-            <h2 className='second-title'>Suck my dick</h2>
-        </div>
-    );
+export default class Hero extends React.Component {
+
+    render() {
+        return(
+            <div className='container' >
+                <h1 className='first-title'>WE RULE THEN ALL</h1>
+                <h2 className='second-title'>Suck my dick</h2>
+            </div>
+        );
+    }
 }
