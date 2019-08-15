c31d0558be519ae4a8d5cb033956ed8d4255da58
John Park <john.suk@descomplica.com.br>
2019-08-15 19:59:02 -0300

    Fixing Slick Slider

--- project/src/components/categories/index.js
+++ project/src/components/categories/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import Slider from 'react-slick'
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import './index.scss'
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 21500
+}
 export default class Categories extends React.Component {
 
   render() {
@@ -10,10 +25,20 @@ export default class Categories extends React.Component {
       className
     } = this.props
 
+
   return(
     <div id={id} className={className} >
-      <h1 className='first-title'>CATEGORIES</h1>
-      <h2 className='second-title'>DANNY DEVITO</h2>
+        <Slider className='slider-categorias' {...settings}>
+          <div className='item-1'>
+            <h3>1</h3>
+          </div>
+          <div className='item-2'>
+            <h3>2</h3>
+          </div>
+          <div className='item-3'>
+            <h3>3</h3>
+          </div>
+        </Slider>
     </div>
   );
 }
