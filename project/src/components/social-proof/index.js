d4d1a2a5043d7186304c31c7b5900f8265c230c7
Davi Goulart <davi.goulartsa@gmail.com>
2019-08-16 00:53:49 -0300

    changing copy from social-proof

--- project/src/components/social-proof/index.js
+++ project/src/components/social-proof/index.js
@@ -3,6 +3,10 @@ import Slider from 'react-slick'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import livia from '../../images/V5-livia.png';
+import barbara from '../../images/V5-barbara.png';
+import matheus from '../../images/V5-matheus.png';
+
 
 import './index.scss'
 
@@ -23,19 +27,19 @@ export default class SocialProof extends React.Component {
             <section className='central-social-proof'>
               <Slider className='slider-social' {...settings}>
                 <div className='item-1-social'>
-                    <img src='https://d3uyk7qgi7fgpo.cloudfront.net/landings/images/355376b8-95ba-4572-8258-f3760149595a-aluno-3.png' />
-                    <p>Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. A ordem dos tratores não altera o pão duris. Suco de cevadiss deixa as pessoas mais interessantis.  Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.</p>
-                    <span>Mussum</span>
+                    <img src={ barbara } />
+                    <p>"Muito bom! O melhor custo benefício do mercado!!! É claro que existe sempre um professor melhor, uma matéria da qual gostamos mais etc!! Mas, no geral o curso é muito bom e tem correspondido as expectativas!! Recomendo o curso para quem deseja se aperfeiçoar!"</p>
+                    <span>Barbara Barbosa</span>
                 </div>
                 <div className='item-2-social'>
-                    <img src='https://d3uyk7qgi7fgpo.cloudfront.net/landings/images/8ca16c2c-07ea-4e11-ac52-bbe28aa80f03-aluno-6.png' />
-                    <p>Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. A ordem dos tratores não altera o pão duris. Suco de cevadiss deixa as pessoas mais interessantis.  Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.</p>
-                    <span>Mussum</span>
+                    <img src={ matheus } />
+                    <p>"Excelente curso! Além de ótimas aulas, o curso é extremamente sério. Recomendo à todos que desejam um curso com ótimo custo benefício e qualidade. Obg."</p>
+                    <span>Matheus Rubello</span>
                 </div>
                 <div className='item-3-social'>
-                    <img src='https://d3uyk7qgi7fgpo.cloudfront.net/landings/images/4ba33bf3-0244-4db7-8356-fb8e26b450ec-aluna-1.png' />
-                    <p>Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. A ordem dos tratores não altera o pão duris. Suco de cevadiss deixa as pessoas mais interessantis.  Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.</p>
-                    <span>Mussum</span>
+                    <img src={ livia } />
+                    <p>“O descomplica foi essencial no meu processo de aprendizagem e esclarecimento de dúvidas durante toda minha jornada para aprovação no enem, super indico.”</p>
+                    <span>Lívia Albuquerque</span>
                 </div>
               </Slider>
               <button className='green-button'>Começar a estudar</button>
