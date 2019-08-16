d43ce5379ea8addde53f508ab21c47d1ee86fb3f
John Park <john.suk@descomplica.com.br>
2019-08-16 10:41:11 -0300

    Cleaning

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import AcessibilityBar from './components/acessibility-bar/index'
 import Hero from './components/hero/index'
 import Categories from './components/categories';
 import Features from './components/features';
@@ -13,7 +12,7 @@ import { animateScroll } from 'react-scroll'
 import './App.scss'
 
 export default class App extends React.Component {
-  
+
   scrollTo () {
     const element = document.querySelector("#categories")
 
@@ -21,8 +20,6 @@ export default class App extends React.Component {
       animateScroll.scrollTo(element.offsetTop)
     }
   }
-   
-    
 
  render() {
   return (
