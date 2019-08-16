5b97baaa29977d5c79ac7ebed43ae165065c8492
Davi Goulart <davi.goulartsa@gmail.com>
2019-08-15 21:00:17 -0300

    putting our beautiful fonts

--- project/src/App.js
+++ project/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Hero from './components/hero/index'
 import Categories from './components/categories';
+import Features from './components/features';
 import SocialProof from './components/social-proof';
 import TryMe from './components/try-me';
 import Faq from './components/faq';
@@ -25,6 +26,7 @@ export default class App extends React.Component {
     <div className="app">
       <Hero id={'hero'} className={'container-hero'} onClick={this.scrollTo}/>
       <Categories id={'categories'} className={'container-categories'} />
+      <Features />
       <Faq />
       <SocialProof />
       <TryMe />
