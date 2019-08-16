738f8913fd28fe3c557de1e49c0d8114f6fca532
Davi Goulart <davi.goulartsa@gmail.com>
2019-08-15 22:56:06 -0300

    Comecando social proof

--- project/src/App.js
+++ project/src/App.js
@@ -6,7 +6,7 @@ import Features from './components/features';
 import SocialProof from './components/social-proof';
 import TryMe from './components/try-me';
 import Faq from './components/faq';
-// import Footer from './components/footer';
+import Footer from './components/footer';
 
 import { animateScroll } from 'react-scroll'
 import './App.scss'
@@ -24,13 +24,13 @@ export default class App extends React.Component {
  render() {
   return (
     <div className="app">
-      <Hero id={'hero'} className={'container-hero'} onClick={this.scrollTo}/>
+      {/* <Hero id={'hero'} className={'container-hero'} onClick={this.scrollTo}/>
       <Categories id={'categories'} className={'container-categories'} />
       <Features />
-      <Faq />
+      <Faq /> */}
       <SocialProof />
-      <TryMe />
-      {/* <Footer /> */}
+      {/* <TryMe />
+      <Footer /> */}
     </div>
   );
   }
